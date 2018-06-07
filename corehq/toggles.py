25371312809debf4a01b797d1df63ce1318f430c
Ethan Soergel <esoergel@gmail.com>
2018-06-07 15:15:53 -0400

    Remove toggle and checkbox to restrict by location

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -809,14 +809,6 @@ MOBILE_UCR = StaticToggle(
     always_enabled={'icds-cas'}
 )
 
-RESTRICT_WEB_USERS_BY_LOCATION = StaticToggle(
-    'restrict_web_users_by_location',
-    "(Deprecated) Allow project to restrict web user permissions by location",
-    TAG_DEPRECATED,
-    namespaces=[NAMESPACE_DOMAIN],
-    description="Don't enable this flag."
-)
-
 API_THROTTLE_WHITELIST = StaticToggle(
     'api_throttle_whitelist',
     ('API throttle whitelist'),
